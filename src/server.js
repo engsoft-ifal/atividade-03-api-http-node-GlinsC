e8b8e1372bfd810a5c1245d7c0383d86750ff30e
Gabriel Lins <123127213+GlinsC@users.noreply.github.com>
2026-02-26 20:30:33 -0300

    Fix JSON.stringify call for protocolos response

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const servidor = http.createServer((req, res) => {
     if (req.method === "GET" && req.url === "/protocolos") {
         res.writeHead(200, { "Content-type": "application/json" })
 
-        res.end(JSON.stringify({ protocolos }))
+        res.end(JSON.stringify( protocolos ))
         return
     }
 
